6c187276622147d10aeeac919503935b983f8d13
HuyGiaJerry <huyfin2004@gmail.com>
2024-09-28 08:01:10 +0700

    Nop bai tap thuc hanh nhom 2 ngay 28/9 , Quan Ly Sinh Vien

---
+++ .NET/N2_QuanLySinhVien/N2_QuanLySinhVien/DanhSachSinhVien.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N2_QuanLySinhVien
+{
+    internal class DanhSachSinhVien
+    {
+        private List<SinhVien> sv = new List<SinhVien>();
+
+        public void addSinhVien(SinhVien s)
+        {
+            sv.Add(s);
+        }
+        public void displayDanhSachSV() 
+        {
+            foreach (SinhVien s in sv)
+            {
+                s.display();
+            }
+        }
+
+        public bool findSvByMaSv(String maSv)
+        {
+            SinhVien s = new SinhVien();
+            bool found = false;
+            foreach (SinhVien sinh in sv)
+            {
+                if(sinh.getMaSv().Equals(maSv) )
+                {
+                    s = sinh;
+                    found = true;
+                }
+
+            }
+            if(found)
+            {
+                Console.WriteLine("--Thong tin Sv Tim Kiem--");
+                s.display();
+            }
+            return found; 
+            
+        }
+
+        public SinhVien maxDtb()
+        {
+            SinhVien s = new SinhVien();
+            double maxDtb = 0;
+            foreach(SinhVien sinh in sv)
+            {
+                if(sinh.getDtb() > maxDtb )
+                    maxDtb = sinh.getDtb();
+            }
+            foreach(SinhVien sinh in sv)
+            {
+                if(sinh.getDtb() == maxDtb)
+                    s = sinh;
+            }
+            return s;
+        }
+
+    }
+}
