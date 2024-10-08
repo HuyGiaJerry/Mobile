63a6216c9e373e0b48742d342b5d5a2fb26090e4
HuyGiaJerry <huyfin2004@gmail.com>
2024-10-08 09:44:03 +0700

    Nop bai tap thuc hanh nhom 2 , Quan ly Phuong Tien Giao Thong 8/10

---
+++ .NET/ThucHanh02_QLGT/ThucHanh02_QLGT/IThongTinThem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThucHanh02_QLGT
+{
+    public interface IThongTinThem
+    {
+        double TocDoToiDa();
+        double MucTieuThuNguyenLieu();
+    }
+}
---
+++ .NET/ThucHanh02_QLGT/ThucHanh02_QLGT/XeHoi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThucHanh02_QLGT
+{
+    public  class XeHoi : PhuongTien
+    {
+        public XeHoi() { }
+        public XeHoi(string tenPT,string loaiNhienLieu) : base(tenPT, loaiNhienLieu)   { }
+
+        public override void DiChuyen()
+        {
+            Console.WriteLine("Xe hoi dang chay");
+
+        }
+
+        public override double MucTieuThuNguyenLieu()
+        {
+            string str = LoaiNhienLieu.ToLower();
+            int choosen = 0;
+            if (str.Equals("ron")) choosen = 22;
+            else if (str.Equals("a95")) choosen = 23;
+            else if (str.Equals("e5")) choosen = 24;
+            else choosen  = 20;
+            return choosen * 2;
+        }
+
+        public override double TocDoToiDa()
+        {
+            return 120;
+        }
+    }
+}
